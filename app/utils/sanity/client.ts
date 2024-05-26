26a8114f8e2238956211c2255b201824911ab879
tatsumiakahori <seadragon0828@gmail.com>
2024-05-26 15:15:23 +0900

    本番でもデータ取れるように修正

--- app/utils/sanity/client.ts
+++ app/utils/sanity/client.ts
@@ -5,5 +5,5 @@ export const client = createClient({
    dataset: "production",
    apiVersion: "2024-03-11",
    // Set to `true` for production environments
-   useCdn: false,
+   useCdn: true,
 })
