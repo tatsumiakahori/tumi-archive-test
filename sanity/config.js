d219993f05c1b8ac923adb60ade3e15327d8f76c
tatsumiakahori <seadragon0828@gmail.com>
2024-06-01 08:21:56 +0900

    cdn false

--- sanity/config.js
+++ sanity/config.js
@@ -4,7 +4,7 @@ export const config = {
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   apiVersion: "2024-03-11",
-  useCdn: process.env.NODE_ENV === "production",
+  useCdn: false,
   token: process.env.SANITY_API_TOKEN,
 };
 
